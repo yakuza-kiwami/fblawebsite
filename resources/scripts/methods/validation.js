96afa5f644cbe1ea802b015b252ace433147613b
yakuza-kiwami <mzqkhan40@gmail.com>
2025-02-05 18:50:52 -0500

    those who understand it now

--- resources/scripts/methods/validation.js
+++ resources/scripts/methods/validation.js
@@ -6,9 +6,9 @@ function schoolSignin(event) {
   let correctPswd = "mango";
   if (inputUser == correctUser && inputPswd == correctPswd) {
     alert("Welcome! You have successfully logged in.");
-    document.getElementById("authForm").style.display = "none";
     // document.getElementById("container").style.display = "block";
     $("#container").fadeIn();
+    document.getElementById("authForm").style.display = "none";
   } else if (inputUser == "" || inputPswd == "") {
     alert("Please enter both your username and password.");
   } else {
@@ -24,13 +24,13 @@ function employerSignin(event) {
   let correctPswd = "thosewhoknow";
   if (inputUser == correctUser && inputPswd == correctPswd) {
     alert("Welcome! You have successfully logged in.");
-    document.getElementById("authForm").style.display = "none";
     // document.getElementById("newposting").style.display = "block";
     // document.getElementById("postings").style.display = "block";
     // document.getElementById("applicants").style.display = "block";
     $("#newposting").fadeIn();
     $("#postings").fadeIn();
     $("#applicants").fadeIn();
+    document.getElementById("authForm").style.display = "none";
   } else if (inputUser == "" || inputPswd == "") {
     alert("Please enter both your username and password.");
   } else {
