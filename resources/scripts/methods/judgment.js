968b7ee6b2261e5a5b4a7ca3864b9f47162c602c
yakuza-kiwami <mzqkhan40@gmail.com>
2025-02-05 20:21:09 -0500

    bullshit manuevers GO

--- resources/scripts/methods/judgment.js
+++ resources/scripts/methods/judgment.js
@@ -1,26 +1,34 @@
-const form = document.getElementById('submission'); // Replace with your form's ID
-
 /* Manage applications and postings */
-function approvePosting() {
+function approvePosting() { // Schools approving postings
   alert("Posting approved!")
   document.getElementById("posting").style.display = "none";
 }
 
-function rejectPosting() {
+function rejectPosting() { // Schools rejecting postings
   alert("Posting rejected.")
-  document.getElementById("posting").style.display = "none";
+  document.getElementById("posting4").style.display = "none";
 }
 
-function removePosting() {
+function removePosting() { // Employers removing postings
   alert("Posting removed.")
   document.getElementById("posting").style.display = "none";
 }
 
-function submitPost() {
+function submitPost() { // Employers submitting postings
   alert("Posting submitted, and pending approval from school administration.");
 };
 
 /* Manage applications */
-function submitApp() {
+function submitApp() { // Students submitting applications
   alert("Application submitted, and pending approval from the employer!");
+}
+
+function approveApp() { // Employers accepting applications
+  alert("Application approved! Contact this applicant using their email or phone number.")
+  document.getElementById("app").style.display = "none";
+}
+
+function rejectApp() { // Employers rejecting applications
+  alert("Application rejected.")
+  document.getElementById("app2").style.display = "none";
 }
