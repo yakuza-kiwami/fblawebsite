cadcdccbfe5159b77be401be6aca42e2634555a9
yakuza-kiwami <mzqkhan40@gmail.com>
2025-02-05 19:18:56 -0500

    Update validation.js

--- resources/scripts/methods/validation.js
+++ resources/scripts/methods/validation.js
@@ -1,13 +1,13 @@
 function schoolSignin() {
   let inputUser = document.getElementById("validateAdmin").value;
   let inputPswd = document.getElementById("validateAdminPwd").value;
-  let correctUser = "fcps";
+  let correctUser = "jonkler";
   let correctPswd = "mango";
   if (inputUser == correctUser && inputPswd == correctPswd) {
     alert("Welcome! You have successfully logged in.");
-    // document.getElementById("container").style.display = "block";
+    $("authForm").fadeOut();
     $("#container").fadeIn();
-    document.getElementById("authForm").style.display = "none";
+    document.getElementById("login").style.display = "none";
   } else if (inputUser == "" || inputPswd == "") {
     alert("Please enter both your username and password.");
   } else {
@@ -18,17 +18,15 @@ function schoolSignin() {
 function employerSignin() {
   let inputUser = document.getElementById("validateEmployer").value;
   let inputPswd = document.getElementById("validateEmployerPwd").value;
-  let correctUser = "khanaz-kp";
+  let correctUser = "whysoserious";
   let correctPswd = "thosewhoknow";
   if (inputUser == correctUser && inputPswd == correctPswd) {
     alert("Welcome! You have successfully logged in.");
-    // document.getElementById("newposting").style.display = "block";
-    // document.getElementById("postings").style.display = "block";
-    // document.getElementById("applicants").style.display = "block";
+    $("authForm").fadeOut();
     $("#newposting").fadeIn();
     $("#postings").fadeIn();
     $("#applicants").fadeIn();
-    document.getElementById("authForm").style.display = "none";
+    document.getElementById("login").style.display = "none";
   } else if (inputUser == "" || inputPswd == "") {
     alert("Please enter both your username and password.");
   } else {
